58fe546958af17c4aed1c6780ad827e91d22e07c
Leonid Haidash <Gaydashleo@gmail.com>
2022-11-05 19:03:29 +0200

    NEWstart

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store, persistor } from 'redux/store.js';
-import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter } from 'react-router-dom';
-import  App  from './App';
+import { App } from 'components/App';
 import './index.css';
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <App />
   </React.StrictMode>
-  );
+);
--- src/redux/contacts/index.js
+++
@@ -1,3 +0,0 @@
-export * as contactsOperations from './contacts-operation';
-export * as contactsActions from './contacts-action';
-export * as contactsSelectors from './contacts-selectors';
