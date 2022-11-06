5836ce3ac1765c44ce91380ae92415d39bd7d9c5
Leonid Haidash <Gaydashleo@gmail.com>
2022-11-06 10:03:27 +0200

    Apptocomponents

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import {Filter}  from 'components/Filter/Filter';
+import { ContactList } from 'components/ContactList/ContactList';
+import { Container, Section, Titleh1, Titleh2 } from './App.styled';
+
+export function App() {
+  
+    return (
+      <Container>
+        <Section title="Phonebook">
+          <Titleh1>Phonebook</Titleh1>
+          <ContactForm />
+        </Section>
+        <Section title="Contacts">
+          <Titleh2>Contacts</Titleh2>
+          <Filter  />
+          <ContactList />
+        </Section>
+      </Container>
+    );
+  }
+
