4d6de363b81069367eb8eca0230e22766ad46ad3
Leonid Haidash <Gaydashleo@gmail.com>
2022-11-08 11:51:10 +0200

    1

--- src/components/AuthNav/AuthNav.styled.jsx
+++ src/components/AuthNav/AuthNav.styled.jsx
@@ -1,15 +1,18 @@
-import styled from "styled-components";
+import styled from "@emotion/styled";
 import { NavLink } from 'react-router-dom';
 
 export const AuthNavLink = styled(NavLink)`
   display: inline-block;
   text-decoration: none;
-  padding: 12px;
+  padding: 14px;
   font-weight: 700;
-  color: #2a363b;
+  color: #33C1FF;
   &:hover,
   &:focus {
-    color: #d95c5c;
-    font-weight: 700;
+    color: #3336FF;
+    font-weight: 900;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
+    rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
   }
 `;
--- src/components/Navigation/Navigation.styled.jsx
+++ src/components/Navigation/Navigation.styled.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "@emotion/styled";
 import { NavLink } from 'react-router-dom';
 
 export const NavigationLink = styled(NavLink)`
@@ -6,14 +6,17 @@ export const NavigationLink = styled(NavLink)`
   text-decoration: none;
   padding: 12px;
   font-weight: 700;
-  color: black;
+  color:#33C1FF;
   &:hover,
   &:focus {
-    color: #d95c5c;
-    font-weight: 700;
+   color: #3336FF;
+    font-weight: 900;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
+    rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
   }
   &.active {
-    color: #d95c5c;
+    color: #3336FF;
 
   }
 `;
